b10516e8c6bb5bd164bf60af3b7f0512a75627a9
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-07-20 23:49:48 +1000

    added savetoFile
    savetofile will add all passwords to a file
    ***NOT ENCRYPTED YET***

--- Serializer.cs
+++ Serializer.cs
@@ -3,27 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace WinFormsApp1
 {
-    internal class Serializer
+    internal static class Serializer
     {
-        public int SaveToFile()
+        public static int SaveToFile(Form1 form)
+        {
+            string saveLocation = "../../../Shadow.png";
+
+            List<passwordInfo> passwords = new();
+            passwords.Add(new passwordInfo("google", "thetruecool", "password123", form));
+            passwords.Add(new passwordInfo("yandex", "thetruecool", "password123", form));
+            passwords.Add(new passwordInfo("outlook", "thetruecool", "password123", form));
+            passwords.Add(new passwordInfo("github", "thetruecool", "password123", form));
+            passwords.Add(new passwordInfo("typingclub", "thetruecool", "password123", form));
+
+            FileStream Save = File.OpenWrite(saveLocation);
+            string bytes = "";
+
+            foreach (passwordInfo password in passwords)
+            {
+                bytes += Serialize(password);
+            }
+
+            Save.Write(Encoding.ASCII.GetBytes(bytes));
+            Save.Close();
+
+            return 1;
+        }
+
+        public static int LoadFromFile()
         {
             throw new NotImplementedException();
         }
 
-        public int LoadFromFile()
+        private static string Serialize(passwordInfo password)
         {
-            throw new NotImplementedException();
+            string text = string.Format("{0}\n{1}\n{2}\n\n",password.WebSite,password.Username,password.Password);
+            return text;
         }
 
-        private void Serialize()
-        {
-            throw new NotImplementedException();
-        }
-
-        private void DeSerialise()
+        private static byte[] DeSerialise(passwordInfo password)
         {
             throw new NotImplementedException();
         }
