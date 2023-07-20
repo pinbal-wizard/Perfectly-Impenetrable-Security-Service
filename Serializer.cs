3e9f5275a469a78d0cc46f0de72c976ce285c3a2
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-07-20 22:40:03 +1000

    Inital Commit
    Initial Commit with names of functions implemented

---
+++ Serializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class Serializer
+    {
+        public int SaveToFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int LoadFromFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Serialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeSerialise()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
