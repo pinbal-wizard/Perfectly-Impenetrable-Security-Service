bc8387a34ac7b68680fcd1d699f4a23a915267d4
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-07-20 23:49:11 +1000

    added saving when closing
    will run form1_deactivate when the form is exiting

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,11 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        private void Form1_Deactivate(Object sender, EventArgs e)
+        {
+            Serializer.SaveToFile(this);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
             sidePanel = new FlowLayoutPanel();
@@ -51,6 +56,7 @@ namespace WinFormsApp1
             infoDisplay = new passwordInfoDisplay(this);
             this.Controls.Add(sidePanel);
             this.Controls.Add(infoDisplay);
+            this.FormClosing += Form1_Deactivate;
             
         }
 
