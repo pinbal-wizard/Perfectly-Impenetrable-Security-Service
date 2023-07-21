1e449f5087a8ee304ca9f8f33b6cd1ce50941e27
grid-zero <produde101360noscope@gmail.com>
2023-07-21 12:13:36 +1000

    added namespaces for lin(s)uks

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
 namespace WinFormsApp1
 {
     internal static class Program
