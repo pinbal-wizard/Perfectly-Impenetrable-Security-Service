32690d87e0c9d6e6f930807270a39872cbd2d695
pinbal-wizard <68714330+pinbal-wizard@users.noreply.github.com>
2023-07-27 22:41:49 +1000

    Manually Merged Password-Popup-Window
    Woeie people be mergeing main without looking

--- MasterPasswordPopup.cs
+++ MasterPasswordPopup.cs
@@ -16,10 +16,40 @@ namespace WinFormsApp1
     public partial class MasterPasswordPopup : Form
     {
         private string password = "123";
+        private bool showPassword = false; // flag to indicate whether the password is visible
 
         public MasterPasswordPopup()
         {
             InitializeComponent();
+            PasswordTextBox.PasswordChar = '*'; // Censor the password by default
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showPassword = !showPassword;
+
+            if (showPassword)
+            {
+                ShowPassword();
+                button1.Text = "Hide Password";
+            }
+            else
+            {
+                HidePassword();
+                button1.Text = "Show Password";
+            }
+        }
+
+        private void ShowPassword()
+        {
+            // Show the password in letters
+            PasswordTextBox.PasswordChar = '\0'; // Set to '\0' (null character) to show letters
+        }
+
+        private void HidePassword()
+        {
+            // Censor the password with asterisks
+            PasswordTextBox.PasswordChar = '*';
         }
 
         private void SubmitPassBtn_Click(object sender, EventArgs e)
@@ -31,9 +61,11 @@ namespace WinFormsApp1
             else
             {
                 MessageBox.Show("Password is incorrect");
-
             }
+        }
 
+        private void Popup_Load(object sender, EventArgs e)
+        {
 
         }
     }
